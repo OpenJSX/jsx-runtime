372b8f09454ffee0bc2e8366e2f479e300d99051
Arthur Stolyar <nekr.fabula@gmail.com>
2015-07-16 00:06:48 +0300

    Update reamde and add API definition

--- runtime.d.ts
+++ runtime.d.ts
@@ -1,60 +1,3 @@
-## JSX IR Runtime
-
-Runtime for rendering JSX-IR. Runtime does not renders JSX-IR, otherwise it provides common interfaces for Renderer to be implemented.
-
-### Installation
-
-```npm install jsx-runtime```
-
-### Usage
-
-This package does not really intended to be used by the end user, instead users should install Renderers which depends on this package.
-
-Runtime 1 main and most important method,  registers an Renderer to the runtime:
-
-```js
-  jsx.register(rendererName, rendererConfig);
-```
-
-Simple Renderer may look like this:
-```js
-var jsx = require('../');
-
-var renderer = jsx.register('jsx-unwrap', {
-  tags: {
-    '*': {
-      enter: function(tag, props) {
-        return {
-          tag: tag,
-          props: props || null,
-          children: []
-        };
-      },
-      leave: function(parent, tag) {
-        if (!parent.children.length) {
-          parent.children = null;
-        }
-
-        return parent;
-      },
-      child: function(child, parent) {
-        parent.children.push(child);
-        return parent;
-      }
-    }
-  }
-});
-
-module.exports = renderer;
-```
-_https://github.com/jsx-ir/jsx-runtime/blob/master/tests/_unwrap-renderer.js_
-
-This simple Renderer from tests just unwraps incoming data back to JSX-IR so it could be easy testable. Renderers could be used in many different ways, starting from generating simple string-markup to complex output of framework objects, bypassing string/dom parsing and initializing directly with passed values.
-
-### API
-
-_Definition in TypeScript format_
-```typescript
 /// <reference path="https://github.com/jsx-ir/spec/blob/master/spec.d.ts" />
 
 interface Runtime {
@@ -123,8 +66,3 @@ interface Renderer {
   // Computes JSX.Element from giving parameters and renders simple tag
   render(tag: string, props?: JSX.Props, children?: JSX.Children);
 }
-```
-
-### License
-
-[MIT](LICENSE.md)
