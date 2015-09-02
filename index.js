d5cd167d28f7684aacdaaa2f621ace414e49216d
Arthur Stolyar <nekr.fabula@gmail.com>
2015-09-02 20:44:42 +0300

    Basic implementation of "overrides"
    First step, all current tests passed

--- index.js
+++ index.js
@@ -2,17 +2,19 @@
 
 var Renderer = require('./lib/renderer');
 var Interpreter = require('./lib/interpreter');
+var overrides = require('./lib/overrides');
+
 var renderers = {};
 
 var jsx = {
   register: function registerRenderer(name, config) {
     name = name.toLowerCase();
 
-    var interpreter = new Interpreter(name, config.tags);
-    var renderer = new Renderer(interpreter, {
-      before: config.before,
-      after: config.after,
-      process: config.process
+    var interpreter = new Interpreter(config);
+    var renderer = new Renderer(overrides.get(name));
+
+    overrides.register(name, interpreter, {
+      weight: 0
     });
 
     renderers[name] = renderer;
@@ -28,6 +30,16 @@ var jsx = {
     }
 
     return renderer.render(tree);
+  },
+
+  override: function override(config, target) {
+    if (!target) target = '*';
+
+    var interpreter = new Interpreter(config);
+
+    overrides.register(target, interpreter, {
+      weight: 1
+    });
   }
 };
 
